63a4f5d95eac491bdf132066e10da930a77be384
amishr45 <amishr45@asu.edu>
2020-11-28 17:10:44 -0700

    changed data

--- data_parser_2.py
+++ data_parser_2.py
@@ -17,10 +17,9 @@ def parse_mat_data():
     insulin_data = list(zip(mat_data['actBolusDelivered'][0], convert_date_str_to_date_obj(mat_data['dateMuBolusStr'])))
     # mapped_cgm_insulin_data = get_data_mapped_by_time(cgm_data, insulin_data)
     mapped_cgm_meal = insulin_to_meal()
-    print(mapped_cgm_meal)
     with open('data/cgm_to_meal_start.csv', 'w', newline='', encoding='utf-8') as out:
         csv_out = csv.writer(out)
-        csv_out.writerow(['cgm','time_insulin','meal_start','meal'])
+        csv_out.writerow(['cgm','time_insulin','time_cgm','meal_start','meal'])
         for row in mapped_cgm_meal:
             csv_out.writerow(row)
 
@@ -70,17 +69,17 @@ def insulin_to_meal():
             start_time = row[2]
             start_time = datetime.strptime(start_time,'%Y-%m-%d %H:%M:%S')
             end_time = start_time + TIME_CGM_THRESHOLD
-            mapped_label.append((row[0], row[2], 1, 0))
+            mapped_label.append((row[0], row[2],row[3],1, 0))
             for j in range(i+1,len(sorted_data)):
                 if i!=j:
                     X = sorted_data[j][2]
                     if(datetime.strptime(sorted_data[j][2],'%Y-%m-%d %H:%M:%S')>start_time) and (datetime.strptime(sorted_data[j][2],'%Y-%m-%d %H:%M:%S')<=end_time):
-                        mapped_label.append((sorted_data[j][0],sorted_data[j][2],0,1))
+                        mapped_label.append((sorted_data[j][0],sorted_data[j][2],sorted_data[j][3],0,1))
                     else:
                         break
             i = j
         else:
-            mapped_label.append((row[0],row[2],0,0))
+            mapped_label.append((row[0],row[2],row[3],0,0))
             i = i+1
 
     return mapped_label
